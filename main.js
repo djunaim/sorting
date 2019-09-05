37cb27d785dc0b3428b92b55826346b67dcae32e
Monica Djunaidi <modjun12@gmail.com>
2019-09-04 21:45:33 -0500

    added html js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hi');
+//first gets container with matching id 'first year' and makes it hidden when page loads
+document.getElementById('firstYear').style.visibility = 'hidden';
+
+// create event click function that whenever the button id is 'sortButton' it will make the hidden container visible
+const sortClick = (e) => {
+    const sortID = e.target.id;
+    if (sortID === 'sortButton') {
+        document.getElementById('firstYear').style.visibility = 'visible';
+    }    
+}
+
+// added an event listener to the 'sortButton' button that whenever it's clicked, it will run the 'sortClick' function
+document.getElementById('sortButton').addEventListener('click', sortClick);
