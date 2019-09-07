d041aff072aee0b28e83f7c3b27486af6dac036d
Monica Djunaidi <modjun12@gmail.com>
2019-09-07 13:23:05 -0500

    added js

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ document.getElementById('sortButton').addEventListener('click', sortClick);
 
 const printToDOM = (divID, toPrint) => {
     document.getElementById(divID).innerHTML = toPrint;
+    expel();
 } 
 
 const students = [];
@@ -24,12 +25,12 @@ const studentCardPrint = (people) => {
     for (let i=0; i < people.length; i++) {
         const person = people[i];
         domString += `
-        <div class="col-sm-4">
+        <div class="col-sm-4" id="${person.studentID}">
             <div class="card studentCard">
                 <div class="card-body">
                     <h5 class="card-title">${person.name}</h5>
                     <p class="card-text">${person.house}</p>
-                    <a href="#" class="btn btn-primary">Expel</a>
+                    <a href="#" class="btn btn-primary" class="expelButton">Expel</a>
                 </div>
             </div> 
         </div>       
@@ -47,11 +48,31 @@ const studentSortClick = (e) => {
         // assign value of input type with id 'nameInput' to variable studentsNames 
         const studentName = document.getElementById('nameInput').value; 
         const randomNum = Math.floor(Math.random() * 4 );
-        const randomHouse = house[randomNum]
-        students.push({name: studentName, house: randomHouse});
-        }   
-    studentCardPrint(students);
+        const randomHouse = house[randomNum];
+        students.push({name: studentName, house: randomHouse, studentID: students.length + 1 });
+        } 
+    studentCardPrint(students);  
+}
+
+const expelClick = (e) => {
+    const expelStudent = e.target.parentElement.children[0].innerHTML;
+    console.log(expelStudent);
+    for (let i=0; i < students.length; i++) {
+        console.log(students[i], expelStudent);
+        if (students[i].name === expelStudent) {
+            students.splice(i, 1);
+        }
+        studentCardPrint(students);
     }
+}
     
 
 document.getElementById('studentSortButton').addEventListener('click', studentSortClick);
+const expel = () => {
+    const buttonExpel = document.getElementsByClassName('expelButton');
+    console.log(buttonExpel);
+    for (let i=0; i<buttonExpel.length; i++) {
+        const clickedThis = buttonExpel[i];
+        clickedThis.addEventListener('click', expelClick);
+    }
+}
